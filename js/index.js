d30203cd243e65fd4b429bdd841ddc740ee60223
dotping-me <dotping.hpg@gmail.com>
2026-03-04 15:37:00 +0400

    Setup veeeeryyy minimal player code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // This is the project's entry point
 // Script loaded with defer, so document body should be all set
 
+import Player from "./player.js";
+
+// ----------------
+//   Canvas Stuff
+// ----------------
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -10,7 +16,6 @@ function resizeWindow() {
 }
 
 function nextFrame() {
-    resizeWindow() // Could remove later on for optimization
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw code here
@@ -18,16 +23,31 @@ function nextFrame() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+// --------------
+//   Game Stuff
+// --------------
+
+const player = new Player();
+
+// Catch keys
+var keys = {};
+window.addEventListener("keydown", e => keys[e.key] = true);
+window.addEventListener("keyup", e => keys[e.key] = false); // Unset keys
+
 // Gameplay loop implementation in here
 function gameLoop() {
+    player.update(keys);
     
-    // TODO: Menu screen
     // TODO: Intro cutscene
     // TODO: Actual gameplay loop
     // TODO: Final boss fight
     // TODO: Endgame
 
+    requestAnimationFrame(gameLoop);
 }
 
 window.addEventListener("resize", resizeWindow);
+resizeWindow(); // Initial resize
 nextFrame();
+
+gameLoop();
---
+++ js/player.js
@@ -0,0 +1,19 @@
+class Player {
+    constructor() {
+        this.hp = 100;
+    }
+
+    // Draw the player and shit
+    draw() {
+
+    }
+
+    // Update player attributes such as movement
+    update(keys) {
+        if (keys["w"]) {
+            console.log("AAAAHHHHHHHHHH!!!");
+        }
+    }
+}
+
+export default Player;
